18735f654e5689a1230da6574730124d18a84816
A15162252289 <1539261210@qq.com>
2022-01-12 16:24:18 +0800

    restart project

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import styled from 'styled-components/macro'
 import { Pagination } from '@mui/material'
 import {Stage,Layer,Image} from 'react-konva'
 import useImage from 'use-image';
 import path from 'path';
+const baseUrl = 'https://raw.githubusercontent.com/IhsotasTon/ddtank-demo/master/src/assets/ddtank/'
+const getRealUrl = (path:string):string => {
+  return decodeURI(baseUrl + path) ;
+}
 const BodyWrapper = styled.div`/Users/abc/metasoga/ddtank-demo/src/assets/ddtank/glass
   position:relative;
   width:250px;
   height:312px;
 `
-class URLImage extends React.Component {
-  state = {
-    image: undefined
-  };
-  image: any;
-  componentDidMount() {
-    this.loadImage();
-  }
-  componentDidUpdate(oldProps: { src: any; }) {
-    // if (oldProps.src !== this.props.src) {
-    //   this.loadImage();
-    // }
-  }
-  componentWillUnmount() {
-    this.image.removeEventListener('load', this.handleLoad);
-  }
-  loadImage() {
-    // save to "this" to remove "load" handler on unmount
-    this.image = new window.Image();
-    this.image.src = './assets/ddtank/face/female/eff177/1/show.png';
-    this.image.addEventListener('load', this.handleLoad);
-  }
-  handleLoad = () => {
-    // after setState react-konva will update canvas and redraw the layer
-    // because "image" property is changed
-    this.setState({
-      image: this.image
-    });
-    // if you keep same image object during source updates
-    // you will have to update layer manually:
-    // this.imageNode.getLayer().batchDraw();
-  };
-  render() {
-    return (
-      <Image
-        image={this.state.image}
-      />
-    );
-  }
+const RealImage = function (props:{url:string}) {
+  let [realImg] =useImage(getRealUrl(props.url))
+  return <Image image={realImg} width={250} height={312} x={0} y={0} />
 }
 function App() {
-  let [clothImg, setClothImg] = useImage('')
-  let [faceImage, setFaceImage] = useState(new window.Image())
-  faceImage.src="./assets/ddtank/face/female/eff177/1/show.png"
-  useEffect(() => { 
-    faceImage.onload = () => {
-      setFaceImage(faceImage)
-    }
-  })
-
   return (
     <div className="App">
       <BodyWrapper>
         <Stage width={250} height={312}>
           <Layer>
-            <URLImage></URLImage>
+            <RealImage url={'emoji/female/face130/1/show.png'}></RealImage>
+            <RealImage url={'face/female/eff177/1/show.png'}></RealImage>
+            <RealImage url={'glass/glass197/1/show.png'}></RealImage>
+            <RealImage url={'hair/female/hair177/1/show.png'}></RealImage>
+            <RealImage url={'head/female/head462/1/show.png'}></RealImage>
+            <RealImage url={'cloth/female/cloth410/1/show.png'}></RealImage>
           </Layer>
         </Stage>
-       
-        {/* <img src={require("./assets/ddtank/face/female/eff177/1/show.png")} style={{ position: 'absolute', top: 0, left: 0 }} alt='cloth410' onLoad={() => {
-          
-        }}></img>
-        <img src={require("./assets/ddtank/glass/glass197/1/show.png")} style={{position:'absolute',top:0,left:0}} alt='cloth410'></img>
-        <img src={require("./assets/ddtank/hair/female/hair177/1/show.png")} style={{ position: 'absolute', top: 0, left: 0 }} alt='cloth410'></img>
-        <img src={require("./assets/ddtank/head/female/head462/1/show.png")} style={{position:'absolute',top:0,left:0}} alt='cloth410'></img> */}
       </BodyWrapper>
       <Pagination
         count={10}
