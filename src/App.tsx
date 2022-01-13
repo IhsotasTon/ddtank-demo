c20dc28e4d8790fbc20d00d4af94f023e11fa455
A15162252289 <1539261210@qq.com>
2022-01-13 16:34:39 +0800

    add cardBorder

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,38 @@ import femaleFaceJson from "./config/female_face.json";
 import femaleEmojiJson from "./config/female_emoji.json";
 import femaleHairJson from "./config/female_hair.json";
 import glassJson from "./config/glass.json";
-
+const cardBorderJson = [{
+  id:1,
+},{
+  id:2,
+},{
+  id:3,
+},{
+  id:4,
+},{
+  id:5,
+},{
+  id:6,
+},{
+  id:7,
+},{
+  id:8,
+},{
+  id:9,
+  },]
+const cardBgJson = [{
+  id:1,
+},{
+  id:2,
+},{
+  id:3,
+},{
+  id:4,
+},{
+  id:5,
+},{
+  id:6,
+}]
 const baseUrl = 'https://raw.githubusercontent.com/IhsotasTon/ddtank-demo/master/src/assets/ddtank/'
 
 const getRealUrl = (path:string):string => {
@@ -32,9 +63,13 @@ const BodyWrapper = styled.div`
 const AppWrapper = styled.div`
   display:flex;
 `
-const RealImage = function (props:{url:string}) {
-  let [realImg] =useImage(getRealUrl(props.url),'anonymous')
-  return <Image image={realImg} width={250} height={312} x={0} y={0} />
+const RealImage = function (props:{url:string,isCardbg?:boolean}) {
+  let [realImg] = useImage(getRealUrl(props.url), 'anonymous')
+  let width = props.isCardbg ? 250 : 250;
+  let height = props.isCardbg ? 312 : 312;
+  let x = props.isCardbg ? 0 :15;
+  let y=props.isCardbg ? 0 : -12;
+  return <Image image={realImg} width={width} height={height} x={x} y={y} />
 }
 const ListWrapper = styled.div`
   display:grid;
@@ -48,6 +83,8 @@ interface UserSelected  {
   emoji: string
   glass: string
   hair: string
+  cardBg: string
+  cardBorder:string
 }
 let defaultUserSelected = {
   cloth:"cloth410",
@@ -55,7 +92,9 @@ let defaultUserSelected = {
   face:"eff177",
   emoji:"face130",
   glass:"glass197",
-  hair:"hair177",
+  hair: "hair177",
+  cardBg: '1',
+  cardBorder:'1'
 }
 
 const dataSum = {
@@ -75,11 +114,35 @@ const dataSum = {
     emoji: femaleEmojiJson,
     glass:glassJson
   },
+  cardBg: {
+    cardBgJson
+  },
+  cardBorder: {
+    cardBorderJson
+  }
 
 }
+function getUrl(part:string,gender:string,v:any):string {
+  let url = `${part}${part === "glass" ? '' : '/' + gender}/${v.id}/1/show.png`
+  if (part === 'cardBg'||part === 'cardBorder') {
+    url=part+'/'+v.id+'.png'
+  }
+  return url
+}
 export function ItemList(props: { gender: string, part: string, setUserSelected: any, userSelected: UserSelected }) {
-  const { gender, part,setUserSelected,userSelected } = props;
-  let realData = (dataSum as any)[gender][part]
+  const { gender, part, setUserSelected, userSelected } = props;
+  let realData;
+  switch (part) {
+    case 'cardBg':
+      realData=cardBgJson
+      break;
+    case 'cardBorder':
+      realData = cardBorderJson
+      break;
+    default:
+      realData = (dataSum as any)[gender][part]
+      break;
+  }
   let [page, setPage] = useState(1);
   const PER_PAGE = 24;
   const count = Math.ceil(realData.length / PER_PAGE);
@@ -93,7 +156,7 @@ export function ItemList(props: { gender: string, part: string, setUserSelected:
     <div>
       <ListWrapper >
         {_DATA.currentData().map((v:any) => {
-          return <img src={getRealUrl(`${part}${part === "glass" ? '' : '/' + gender}/${v.id}/1/show.png`)} style={{ display: 'block', width: '100%', height: '100%'}} onClick={() => {
+          return <img src={getRealUrl(getUrl(part,gender,v))} style={{ display: 'block', width: '100%', height: '100%'}} onClick={() => {
             setUserSelected({ ...userSelected, [part]: v.id })
           }} alt={v.id}></img>
         })}
@@ -163,7 +226,9 @@ function randomHeros(gender: string, number: number): any[] {
         glass: glassJson[randFrom0ToN(glassJson.length)].id,
         hair: femaleHairJson[randFrom0ToN(femaleHairJson.length)].id,
         head: femaleHeadJson[randFrom0ToN(femaleHeadJson.length)].id,
-        cloth: femaleClothJson[randFrom0ToN(femaleClothJson.length)].id
+        cloth: femaleClothJson[randFrom0ToN(femaleClothJson.length)].id,
+        cardBg: cardBgJson[randFrom0ToN(cardBgJson.length)].id,
+        cardBorderJson: cardBorderJson[randFrom0ToN(cardBorderJson.length)].id
       })
     }
       
@@ -175,7 +240,9 @@ function randomHeros(gender: string, number: number): any[] {
         glass: glassJson[randFrom0ToN(glassJson.length)].id,
         hair: maleHairJson[randFrom0ToN(maleHairJson.length)].id,
         head: maleHeadJson[randFrom0ToN(maleHeadJson.length)].id,
-        cloth: maleClothJson[randFrom0ToN(maleClothJson.length)].id
+        cloth: maleClothJson[randFrom0ToN(maleClothJson.length)].id,
+        cardBg: cardBgJson[randFrom0ToN(cardBgJson.length)].id,
+        cardBorderJson: cardBorderJson[randFrom0ToN(cardBorderJson.length)].id
       })
     }
   }
@@ -194,21 +261,21 @@ function App() {
     
   },[gender])
   
-  const { head, cloth, hair, emoji, face, glass } = userSelected
+  const { head, cloth, hair, emoji, face, glass,cardBg,cardBorder } = userSelected
   return (
     <div>
     <AppWrapper>
       <BodyWrapper>
         <Stage width={250} height={312}>
             <Layer id='1'>
-            <RealImage url={`cardBg/1.png`}></RealImage>
+            <RealImage url={`cardBg/${cardBg}.png`} isCardbg></RealImage>
             <RealImage url={`emoji/${gender}/${emoji}/1/show.png`}></RealImage>
             <RealImage url={`face/${gender}/${face}/1/show.png`}></RealImage>
             <RealImage url={`glass/${glass}/1/show.png`}></RealImage>
             <RealImage url={`hair/${gender}/${hair}/1/show.png`}></RealImage>
             <RealImage url={`head/${gender}/${head}/1/show.png`}></RealImage>
             <RealImage url={`cloth/${gender}/${cloth}/1/show.png`}></RealImage>
-            <RealImage url={`cardBorder/1.png`}></RealImage>
+            <RealImage url={`cardBorder/${cardBorder}.png`} isCardbg></RealImage>
           </Layer>
         </Stage>
         <SelectedGenderWp>
@@ -231,7 +298,7 @@ function App() {
       </BodyWrapper>
       <RightWrapper>
         <TabWrapper>
-        {['cloth', 'head', 'face', 'emoji', 'glass', 'hair'].map(
+        {['cloth', 'head', 'face', 'emoji', 'glass', 'hair','cardBg','cardBorder'].map(
           (item,idx) => {
             return <Tab onClick={() => {
              setPart(item)
@@ -248,13 +315,15 @@ function App() {
           <div style={{ display:'flex',flexDirection:'column'}}>
             <Stage width={250} height={312}>
               <Layer id={item.emoji}>
+                <RealImage url={`cardBg/${item.cardBg}.png`} isCardbg></RealImage>
                 <RealImage url={`emoji/${gender}/${item.emoji}/1/show.png`}></RealImage>
                 <RealImage url={`emoji/${gender}/${item.emoji}/1/show.png`}></RealImage>
                 <RealImage url={`face/${gender}/${item.face}/1/show.png`}></RealImage>
                 <RealImage url={`glass/${item.glass}/1/show.png`}></RealImage>
                 <RealImage url={`hair/${gender}/${item.hair}/1/show.png`}></RealImage>
                 <RealImage url={`head/${gender}/${item.head}/1/show.png`}></RealImage>
-                  <RealImage url={`cloth/${gender}/${item.cloth}/1/show.png`}></RealImage>
+                <RealImage url={`cloth/${gender}/${item.cloth}/1/show.png`}></RealImage>
+                <RealImage url={`cardBorder/${item.cardBorder}.png`} isCardbg></RealImage>
               </Layer>
             </Stage>
             <div>{`${item.cloth}_${item.face}_${item.glass}_${item.hair}_${item.head}_${item.emoji}|`}</div>
